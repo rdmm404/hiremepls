7a44367e7a88ca41f4995209132a523b1b626172
rdmm123 <robedanilo2001@gmail.com>
2025-03-14 23:04:04 -0400

    disable openapi and docs if prod env

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,9 +4,10 @@ from src.users.api import router as users_router
 from src.auth.api import router as auth_router
 from src.jobs.api import router as jobs_router
 from src.applications.api import router as applications_router
+from src.core.config import settings
 
 
-app = FastAPI()
+app = FastAPI(openapi_url=None if settings.ENVIRONMENT == "prd" else "/api/v1/openapi.json")
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(jobs_router)
