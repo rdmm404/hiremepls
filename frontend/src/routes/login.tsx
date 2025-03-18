dd1f7d64b279d99a6118e1fcabdf7aeeb6b8f82c
rdmm123 <robedanilo2001@gmail.com>
2025-03-17 22:57:38 -0400

    add router and initial login page

---
+++ frontend/src/routes/login.tsx
@@ -0,0 +1,53 @@
+import type React from "react";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/login")({
+  component: LoginForm,
+});
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <form
+        onSubmit={handleSubmit}
+        className="w-full max-w-sm p-6 space-y-6 rounded-lg border border-foreground/10"
+      >
+        <div className="space-y-2">
+          <Input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-foreground/20 placeholder/50"
+            required
+          />
+        </div>
+        <div className="space-y-2">
+          <Input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-foreground/20 placeholder/50"
+            required
+          />
+        </div>
+        <Button type="submit" className="w-full bg-primary hover:bg-primary/80">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,14 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
