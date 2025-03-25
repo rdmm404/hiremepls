061baf3021183a319ed133e62520ba04ee970750
rdmm123 <robedanilo2001@gmail.com>
2025-03-24 22:01:58 -0400

    application card redesign

--- frontend/src/components/ApplicationCard.tsx
+++ frontend/src/components/ApplicationCard.tsx
@@ -1,6 +1,8 @@
-import { MapPin } from "lucide-react";
+import { MapPin, Info, Link as LinkIcon } from "lucide-react";
 
 import { ApplicationSummary } from "@/gen";
+import { Link } from "@tanstack/react-router";
+import { cn } from "@/lib/utils";
 
 interface ApplicationCardProps {
   application: ApplicationSummary;
@@ -8,19 +10,45 @@ interface ApplicationCardProps {
 
 export function ApplicationCard({ application }: ApplicationCardProps) {
   return (
-    <div className="bg-card border-border border rounded-lg p-4">
-      <div className="flex justify-between items-start flex-wrap">
-        <div>
-          <h3 className="text-lg">{application.job.company.name}</h3>
-        </div>
-        <span className="inline-flex gap-1 items-center justify-end">
+    <div className="bg-card border-border border rounded-lg p-4 flex flex-col">
+      <div>
+        <h3>
+          <Link
+            to={"/applications/$applicationId"}
+            params={{ applicationId: application.id.toString() }}
+            className="hover:text-white"
+          >
+            {application.job.job_title}{" "}
+          </Link>
+          <Link
+            to={application.job.company.url || "#"}
+            className={cn(!application.job.company.url && "cursor-auto")}
+          >
+            <span
+              className={cn(
+                application.job.company.url && "hover:text-foreground",
+                "text-muted-foreground text-sm"
+              )}
+            >
+              @ {application.job.company.name}
+            </span>
+          </Link>
+        </h3>
+      </div>
+      <hr className="my-2" />
+      <div className="flex grow gap-2 items-center flex-wrap">
+        <Link to={application.job.job_url} className="hover:text-white">
+          <LinkIcon className="size-4" />
+        </Link>
+        |
+        <span className="inline-flex gap-1 items-center justify-end text-sm">
           <MapPin className="size-4" /> {application.job.location}
         </span>
+        |
+        <span className="inline-flex gap-1 items-center justify-end text-sm">
+          <Info className="size-4" /> {application.status}
+        </span>
       </div>
-      <p className="text-sm">{application.job.job_title}</p>
-      <hr className="my-2" />
-      <p className="mt-0 pt-0">Status: {application.status}</p>
-      {/* <p>{application.job.llm_summary}</p> */}
     </div>
   );
 }
